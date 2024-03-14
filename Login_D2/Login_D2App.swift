8dc3fe59a64c7ff9dbd50f00e84deebf31c4f81d
sirx2713 <bradx2713@gmail.com>
2024-03-14 17:55:50 +0300

    Initial Commit

---
+++ Login_D2/Login_D2App.swift
@@ -0,0 +1,17 @@
+//
+//  Login_D2App.swift
+//  Login_D2
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Login_D2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
